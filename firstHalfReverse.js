d52d3659e3d0c655fbbb2339edeb0644f340ee57
shaheryarshaikh1011 <sherishaikh2611@gmail.com>
2022-08-26 14:22:46 +0000

    reverse first half of string

---
+++ firstHalfReverse.js
@@ -0,0 +1,19 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+let str="Shaheryar";
+function FirstHalfReverse(str)
+{
+    if(str.length%2==0)
+    {
+        numberVal=(str.length/2);
+    }
+    else
+    {
+        numberVal=(str.length/2)+1;
+    }
+    firstHalf=str.substring(0,numberVal);
+    secondHalf=str.substring(numberVal,str.length)
+    totalString=firstHalf.split("").reverse().join("").concat(secondHalf);
+    return totalString
+}
+console.log(FirstHalfReverse(str));
